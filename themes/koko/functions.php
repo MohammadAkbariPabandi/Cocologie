a495f0fe2fe60bb99416eb260b229c35c2e55bf1
Mohammad <remap0095@yahoo.com>
2025-05-21 19:32:00 +0330

    add global Css and Header navbar

--- themes/koko/functions.php
+++ themes/koko/functions.php
@@ -1,5 +1,13 @@
+
 <?php
-function koko_theme_scripts() {
-    wp_enqueue_style('style', get_stylesheet_uri());
+function koko_theme_setup() {
+    // Add theme support (optional features)
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
 }
-add_action('wp_enqueue_scripts', 'koko_theme_scripts');
+add_action('after_setup_theme', 'koko_theme_setup');
+
+function koko_enqueue_styles() {
+    wp_enqueue_style('koko-style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'koko_enqueue_styles');
