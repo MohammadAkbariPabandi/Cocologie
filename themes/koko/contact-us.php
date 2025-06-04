fcda027c7363817440c379c5cae589d8da625397
Mohammad <remap0095@yahoo.com>
2025-06-04 19:39:19 +0330

    add the register page link and complete the contact us page

--- themes/koko/contact-us.php
+++ themes/koko/contact-us.php
@@ -11,6 +11,18 @@ get_header(); ?>
        <?php the_content(); ?>
     </div>
      
+    <div class="contactInfo">
+      <h2 class="base mb-5">اطلاعات تماس</h2>
+      <h3 class="mb-3 base">آدرس:</h3>
+      <p class="base">تهران، یوسف آباد خیابان اسدآبادی، بالاتر از پمپ بنزین، ساختمان 501، طبقه 4، واحد 13</p>
+      <h3 class="mb-3 base">تلفن:</h3>
+      <p class="base"> 021-88222222</p>
+      <h3 class="mb-3 base">همراه:</h3>
+      <p class="base">0921-0031737</p>
+      <h3 class="mb-3 base">ایمیل:</h3>
+      <p class="base">moslemifar.pdh@gmail.com</p>
+    </div>
+
   </div>
 </div>
 
