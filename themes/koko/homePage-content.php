50328b2a0789e1b6776615f7318b294ac294556c
Mohammad <remap0095@yahoo.com>
2025-08-15 19:34:04 +0330

    bug fix

--- themes/koko/homePage-content.php
+++ themes/koko/homePage-content.php
@@ -33,7 +33,7 @@
         <h2 class="mb-3 base" style="color: #04446a;">دنیای بازی‌های کوکولوژی</h2>
         <h4 class="base" style="color: #70798B;">
             هر بازی داستان و سبک خاص خودش رو داره، ببین کدوم رو بیشتر دوست داری. از بازی‌های ساده و سرگرم‌کننده تا چالش‌های نفس‌گیر، همه اینجاست!
-        </h2>
+        </h4>
     </div>
     <div class="GameSlider">
         <div class="GameSlider-track">
@@ -77,6 +77,100 @@
             <?php else : ?>
                 <p class="plays-not-found">No Game found.</p>
             <?php endif; ?>
+        </div>
+    </div>
+
+</section>
+
+<section class="Panel">
+    <div class="container Panel-content">
+        <div class="row">
+            <div class="col-md-6">
+                <h2 class="mb-3 base" style="color: #FFF;">پنل سازمانی و ثبت سفارش گروهی</h2>
+                <h4 class="base" style="color: #70798B;">میتوانید سفارشات خود را به صورت گروهی ثبت کنید</h4>
+                <h4 class="base" style="color: #70798B;">مناسب برای رویدادها، مدارس، سازمان‌ها و تیم‌های بزرگ</h4>
+            </div>
+            <div class="col-md-6 Panel-content-button">
+                <a class="PillButton ToryPillButton" href="#">درخواست مشاوره</a>
+            </div>
+        </div>
+    </div>
+    
+</section>
+
+<section class="container kokoGames-section">
+    <h2 class="mb-3 base" style="color: #d5752c;"><?php echo get_theme_field('kokogames-section-title', '', $post->ID); ?></h2>
+    <h2 class="base" style="color: #04446a;">
+        <?php echo get_theme_field('kokogames-section-subtitle'); ?>
+    </h2>
+    <div class="kokoGames-process-cards">
+        <div class="row kokoGames-card">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div class="col-md-4 kokoGames-card-content">
+                    <div class="kokoGame-card-btn">
+                        <a href="<?php the_permalink(); ?>" class="ZodiacButton"><?php echo $wp_query->current_post + 1; ?></a>
+                    </div>
+                    <h2 class="base kokoGames-card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <p class="base"><?php echo get_the_excerpt(); ?></p>
+                </div>
+            <?php endwhile; endif; ?>
+        </div>   
+        <img src="<?php echo get_template_directory_uri(); ?>/images/dash.png" >
+    </div>
+</section>
+
+
+
+<section class="container kokoPlay-section">
+    <div class="row">
+        <div class="col-md-4 kokoPlay-header">
+            <h4 class="base">رویکرد ما</h4>
+            <h2 class="base" style="color: #70798B;">راه حل های<span>
+            <h1 style="color: #04446a;">متناسب با نیاز شما</h1></span></h2>
+            <p class="mt-5 base body-base" style="color: #70798B;">
+               کوکولوژی با هدف ترکیب علم روانشناسی و سرگرمی تأسیس شده است. ما معتقدیم که یادگیری و خودشناسی می‌تواند لذت‌بخش و سرگرم‌کننده باشد. تیم ما متشکل از روانشناسان متخصص و طراحان بازی است که با همکاری یکدیگر، تجربه‌ای منحصر به فرد برای شما فراهم کرده‌اند. هدف ما کمک به افراد برای شناخت بهتر خود و بهبود روابط بین فردی از طریق بازی‌ها روانشناسی است.
+            </p>
+        </div>
+        <div class="col-md-5 kokoPlay-image">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/games.png" >
+        </div>
+        <div class="col-md-3 kokoPlay-list">
+            <?php
+            // Query for Games posts
+            $games_args = array(
+                'post_type'      => 'Game',
+                'posts_per_page' => 3,
+                'orderby'        => 'title',
+                'order'          => 'ASC'
+            );
+
+            $games_query = new WP_Query($games_args);
+
+            if ($games_query->have_posts()) :
+                echo '<div class="plays-card-container">';
+                while ($games_query->have_posts()) : $games_query->the_post();
+                    ?>
+                    <a href="<?php the_permalink(); ?>">
+                        <div class="play-card">
+                            <h3 class="base play-title" style="color: #04446a;"><?php the_title(); ?></h3>
+                            <?php if (has_post_thumbnail()) : ?>
+                                <div class="play-thumbnail">
+                                    <?php the_post_thumbnail('medium'); ?>
+                                </div>
+                            <?php endif; ?>
+                            <div class="play-content base body-small" style="color: #70798B;">
+                                <?php the_excerpt(); ?>
+                            </div>
+                        </div>
+                    </a>
+                    <?php
+                endwhile;
+                echo '</div>';
+                wp_reset_postdata();
+            else :
+                echo '<p>No plays found.</p>';
+            endif;
+            ?>
         </div>
     </div>
 
@@ -173,84 +267,6 @@
         </div>
        
     </div>
-</section>
-
-<section class="container kokoGames-section">
-    <h2 class="mb-3 base" style="color: #d5752c;"><?php echo get_theme_field('kokogames-section-title', '', $post->ID); ?></h2>
-    <h2 class="base" style="color: #04446a;">
-        <?php echo get_theme_field('kokogames-section-subtitle'); ?>
-    </h2>
-    <div class="kokoGames-process-cards">
-        <div class="row kokoGames-card">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <div class="col-md-4 kokoGames-card-content">
-                    <div class="kokoGame-card-btn">
-                        <a href="<?php the_permalink(); ?>" class="ZodiacButton"><?php echo $wp_query->current_post + 1; ?></a>
-                    </div>
-                    <h2 class="base kokoGames-card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <p class="base"><?php echo get_the_excerpt(); ?></p>
-                </div>
-            <?php endwhile; endif; ?>
-        </div>   
-        <img src="<?php echo get_template_directory_uri(); ?>/images/dash.png" >
-    </div>
-</section>
-
-
-
-<section class="container kokoPlay-section">
-    <div class="row">
-        <div class="col-md-4 kokoPlay-header">
-            <h4 class="base">رویکرد ما</h4>
-            <h2 class="base" style="color: #70798B;">راه حل های<span>
-            <h1 style="color: #04446a;">متناسب با نیاز شما</h1></span></h2>
-            <p class="mt-5 base body-base" style="color: #70798B;">
-               کوکولوژی با هدف ترکیب علم روانشناسی و سرگرمی تأسیس شده است. ما معتقدیم که یادگیری و خودشناسی می‌تواند لذت‌بخش و سرگرم‌کننده باشد. تیم ما متشکل از روانشناسان متخصص و طراحان بازی است که با همکاری یکدیگر، تجربه‌ای منحصر به فرد برای شما فراهم کرده‌اند. هدف ما کمک به افراد برای شناخت بهتر خود و بهبود روابط بین فردی از طریق بازی‌ها روانشناسی است.
-            </p>
-        </div>
-        <div class="col-md-5 kokoPlay-image">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/games.png" >
-        </div>
-        <div class="col-md-3 kokoPlay-list">
-            <?php
-            // Query for Games posts
-            $games_args = array(
-                'post_type'      => 'Game',
-                'posts_per_page' => 3,
-                'orderby'        => 'title',
-                'order'          => 'ASC'
-            );
-
-            $games_query = new WP_Query($games_args);
-
-            if ($games_query->have_posts()) :
-                echo '<div class="plays-card-container">';
-                while ($games_query->have_posts()) : $games_query->the_post();
-                    ?>
-                    <a href="<?php the_permalink(); ?>">
-                        <div class="play-card">
-                            <h3 class="base play-title" style="color: #04446a;"><?php the_title(); ?></h3>
-                            <?php if (has_post_thumbnail()) : ?>
-                                <div class="play-thumbnail">
-                                    <?php the_post_thumbnail('medium'); ?>
-                                </div>
-                            <?php endif; ?>
-                            <div class="play-content base body-small" style="color: #70798B;">
-                                <?php the_excerpt(); ?>
-                            </div>
-                        </div>
-                    </a>
-                    <?php
-                endwhile;
-                echo '</div>';
-                wp_reset_postdata();
-            else :
-                echo '<p>No plays found.</p>';
-            endif;
-            ?>
-        </div>
-    </div>
-
 </section>
 
 <section class="faq-section base container">
